2fcf573da0cdeea264943c5554f9033af1258db3
yamskov <myamskov@yandex.ru>
2022-09-25 23:21:48 +0400

    Исправления после ревью

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -13,6 +13,9 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
     List<Item> findByOwner(User owner, Sort sort);
 
-    @Query("select i from Item i where i.available = true and (upper(i.name) like upper(concat('%',?1,'%')) or upper(i.description) like upper(concat('%',?1,'%')))")
+    @Query("select i from Item i " +
+            "where i.available = true " +
+            "and (upper(i.name) like upper(concat('%',?1,'%')) " +
+            "or upper(i.description) like upper(concat('%',?1,'%')))")
     List<Item> searchText(String text);
 }
